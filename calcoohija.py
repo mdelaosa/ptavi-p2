a5fafba20170e95784cb6993f04fe78d7156d8dc
Maria De La Osa Martinez <mdelaosa@alpha13.aulas.gsyc.urjc.es>
2018-09-25 09:58:10 +0200

    calcoohija.py

--- calcoohija.py
+++ calcoohija.py
@@ -8,13 +8,15 @@ Calculadora con clase suma, resta, mult, div.
 
 import sys
 
-class CalculadoraHija:
+class Calculadora:
     
     def plus(self, op1, op2):
         return op1 + op2
     
     def minus(self, op1, op2):
         return op1 - op2
+
+class CalculadoraHija(Calculadora):
     
     def mult(self, op1, op2):
         return op1 * op2
