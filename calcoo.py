5c565567d2c217a8ee56dccaf7bfeb025ca5b8c1
Maria De La Osa Martinez <mdelaosa@gamma18.aulas.gsyc.urjc.es>
2018-10-01 10:42:02 +0200

    Calcoo

--- calcoo.py
+++ calcoo.py
@@ -8,31 +8,33 @@ Calculadora con clase suma y resta.
 
 import sys
 
+
 class Calculadora:
-    
+
     def plus(self, op1, op2):
         return op1 + op2
-    
+
     def minus(self, op1, op2):
         return op1 - op2
-  
+
+
 if __name__ == '__main__':
-    
+
     micalc = Calculadora()
     try:
         op1 = int(sys.argv[1])
         op2 = int(sys.argv[3])
         Suma = micalc.plus(op1, op2)
         Resta = micalc.minus(op1, op2)
-        
+
     except ValueError:
         sys.exit('Error: Non numerical parameters')
-        
+
     if sys.argv[2] == "suma":
         resultado = Suma
     elif sys.argv[2] == "resta":
         resultado = Resta
     else:
         sys.exit('Operación sólo puede ser sumar o restar')
-    
+
     print(resultado)
