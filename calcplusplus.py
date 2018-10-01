780030770915259c7d4c14137a8d339df3d859e8
Maria De La Osa Martinez <mdelaosa@gamma08.aulas.gsyc.urjc.es>
2018-10-01 16:46:39 +0200

    Calc Plus Plus

--- calcplusplus.py
+++ calcplusplus.py
@@ -25,32 +25,24 @@ if __name__ == '__main__':
 
         for line in linea:
             numeros = list(map(int, line[1:]))
-
-            if 'suma' in line:
-                op1 = 0
-                for op2 in numeros:
+            op1 = numeros[0]
+            for op2 in numeros[1:]:
+                if 'suma' in line:
                     op1 = micalc.plus(op1, op2)
 
-            elif 'resta' in line:
-                op1 = numeros[0]
-                for op2 in numeros[1:]:
+                elif 'resta' in line:
                     op1 = micalc.minus(op1, op2)
 
-            elif 'multiplica' in line:
-                op1 = 1
-                for op2 in numeros:
+                elif 'multiplica' in line:
                     op1 = micalc.mult(op1, op2)
 
-            elif 'divide' in line:
-                op1 = numeros[0]
-                try:
-                    for op2 in numeros[1:]:
+                elif 'divide' in line:
+                    if op1 == 0 or op2 == 0:
+                        print('Division by zero is not allowed')
+                    else:
                         op1 = micalc.div(op1, op2)
 
-                except ZeroDivisionError:
-                    print('Division by zero is not allowed')
-
-            else:
-                sys.exit('Operation not valid')
+                else:
+                    sys.exit('Operation not valid')
 
             print(op1)
